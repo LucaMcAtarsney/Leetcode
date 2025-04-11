7c0876a8f63ee89fd06a26642e3148038628ceb5
Luca McAtarsney <luca@Lucas-MacBook-Air.local>
2025-04-11 08:30:11 +0100

    Leetcode 2

--- twoSum_1.py
+++ twoSum_1.py
@@ -1,12 +1,11 @@
-class Solution(object):
-    def twoSum(self, nums, target):
+def twoSum(nums, target):
 
-        num_map = {}
+    num_map = {}
 
-        for i, num in enumerate(nums):
-            complement = target - num
+    for i, num in enumerate(nums):
+        complement = target - num
 
-            if complement in num_map:
-                return [num_map[complement],i]
-            
-            num_map[num] = i
+        if complement in num_map:
+            return [num_map[complement],i]
+        
+        num_map[num] = i
